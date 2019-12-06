409a4988a6820a834915c453e40839e9637ae9db
Brady Bridges <bradyjbridges@gmail.com>
2019-12-06 15:38:52 -0700

    Add endpoints and fetch when app mounts

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from '../../logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { getProjects, getPalettes } from '../../apiCalls';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: [],
+      palettes: [],
+      colors: []
+    }
+  }
+  componentDidMount = async () => {
+    const projects = await getProjects();
+    console.log(projects);
+    const palettes = await getPalettes();
+    console.log(palettes);
+    this.setState({ projects, palettes });
+  }
+
+  render() {
+    return (
+      <h1>Hello</h1>
+    );
+  }
 }
 
 export default App;
