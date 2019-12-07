67c93415cd8c67856ca1c9270a63a4cfdd09b668
Brady Bridges <bradyjbridges@gmail.com>
2019-12-06 17:20:36 -0700

    Add functionality to generate new palettes and skip locked colors

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -9,15 +9,45 @@ export class App extends Component {
     super();
     this.state = {
       isLoading: true,
+      tempColors: [
+        {name: 'color_1', color: '#000000', isLocked: true },
+        {name: 'color_2', color: '#000000', isLocked: false },
+        {name: 'color_3', color: '#000000', isLocked: false },
+        {name: 'color_4', color: '#000000', isLocked: false },
+        {name: 'color_5', color: '#000000', isLocked: false },
+      ],
     };
   }
  
   componentDidMount = async () => {
     const projects = await getProjects();
-    this.props.setProjects(projects);
     const palettes = await getPalettes();
+    this.props.setProjects(projects);
     this.props.setPalettes(palettes);
     this.setState({ isLoading: false });
+  }
+
+  generatePalette = () => {
+    this.state.tempColors.forEach((color, i) => {
+      if(!color.isLocked) {
+        const randomColor = this.generateColor();
+        //this is so we don't modify state directly but use setState
+        let updatedPalette = this.state.tempColors.map((color) => color);
+        updatedPalette[i].color = randomColor;
+        this.setState({ tempColors: updatedPalette });
+      }
+    });
+  }
+
+  generateColor = () => {
+    const options = ['A','B','C','D','E','F','0','1','2','3','4','5','6','7','8','9'];
+    let newColor = [];
+    for(let i = 0; i < 6; i++) { 
+      const randomIndex = Math.floor(Math.random() * options.length);
+      newColor.push(options[randomIndex]);
+    }
+    newColor.unshift('#');
+    return newColor.join('');
   }
 
   render() {
@@ -25,6 +55,7 @@ export class App extends Component {
       <main>
         {this.state.isLoading && <h1>Loading...</h1>}
         <h1>Hello</h1>
+        <button onClick={this.generatePalette}>Generate Palette</button>
       </main>
     );
   }
