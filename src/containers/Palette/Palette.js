705e5f934fac5f82ce3a82ec613457e6c0d1e239
Sara <saralkarsh@gmail.com>
2019-12-07 17:06:41 -0700

    Confirm newly saved palettes render on page

--- src/containers/Palette/Palette.js
+++ src/containers/Palette/Palette.js
@@ -6,11 +6,13 @@ export const Palette = ({ palette_name, color_1, color_2, color_3, color_4, colo
   return (
     <section className='palette'>
       <h6 className='palette-name'>{palette_name}</h6>
-      <div className='color1'>{color_1}</div>
-      <div className='color2'>{color_2}</div>
-      <div className='color3'>{color_3}</div>
-      <div className='color4'>{color_4}</div>
-      <div className='color5'>{color_5}</div>
+      <div className='palette-color-container'>
+        <div className='color1' color={color_1}>{color_1}</div>
+        <div className='color2' color={color_2}>{color_2}</div>
+        <div className='color3' color={color_3}>{color_3}</div>
+        <div className='color4' color={color_4}>{color_4}</div>
+        <div className='color5' color={color_5}>{color_5}</div>
+      </div>
     </section>
   )
 }
