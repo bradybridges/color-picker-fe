2716f28e7d272476da73e38ec74c5ee593364cbe
Sara <saralkarsh@gmail.com>
2019-12-07 17:22:34 -0700

    Get palette colors to display as colors, but some showing up white

--- src/containers/Palette/Palette.js
+++ src/containers/Palette/Palette.js
@@ -3,15 +3,17 @@ import { connect } from 'react-redux'
 import './Palette.css'
 
 export const Palette = ({ palette_name, color_1, color_2, color_3, color_4, color_5}) => {
+  // const style = { backgroundColor: color_1 }
+
   return (
     <section className='palette'>
       <h6 className='palette-name'>{palette_name}</h6>
       <div className='palette-color-container'>
-        <div className='color1' color={color_1}>{color_1}</div>
-        <div className='color2' color={color_2}>{color_2}</div>
-        <div className='color3' color={color_3}>{color_3}</div>
-        <div className='color4' color={color_4}>{color_4}</div>
-        <div className='color5' color={color_5}>{color_5}</div>
+        <div className='color1' style={{backgroundColor: color_1}}>{color_1}</div>
+        <div className='color2' style={{backgroundColor: color_2}}>{color_2}</div>
+        <div className='color3' style={{backgroundColor: color_3}}>{color_3}</div>
+        <div className='color4' sstyle={{backgroundColor: color_4}}>{color_4}</div>
+        <div className='color5' style={{backgroundColor: color_5}}>{color_5}</div>
       </div>
     </section>
   )
