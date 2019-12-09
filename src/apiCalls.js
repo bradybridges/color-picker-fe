cfc4cbd612c61c592fc03798ca1cda16a3ce9c98
Brady Bridges <bradyjbridges@gmail.com>
2019-12-08 18:47:18 -0700

    Lint a little

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,6 @@
 export const getProjects = async () => {
   const response = await fetch('https://color-picker-backend.herokuapp.com/api/v1/projects');
-  if(!response.ok) {
+  if (!response.ok) {
     return Error('Failed to fetch projects');
   }
   const projects = await response.json();
@@ -9,7 +9,7 @@ export const getProjects = async () => {
 
 export const getPalettes = async () => {
   const response = await fetch('https://color-picker-backend.herokuapp.com/api/v1/palettes');
-  if(!response.ok) {
+  if (!response.ok) {
     return Error('Failed to fetch palettes');
   }
   const palettes = await response.json();
@@ -18,7 +18,7 @@ export const getPalettes = async () => {
 
 export const getProject = async (id) => {
   const response = await fetch(`https://color-picker-backend.herokuapp.com/api/v1/projects/${id}`);
-  if(!response.ok) {
+  if (!response.ok) {
     return Error('Failed to fetch pallete');
   }
   const palette = await response.json();
@@ -35,7 +35,7 @@ export const getPalette = async (paletteId) => {
 };
 
 export const postProject = async (name) => {
-  const response = await fetch(`https://color-picker-backend.herokuapp.com/api/v1/projects`, {
+  const response = await fetch('https://color-picker-backend.herokuapp.com/api/v1/projects', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ name }),
@@ -44,7 +44,6 @@ export const postProject = async (name) => {
     return Error('Failed to post new project');
   }
   const message = await response.json();
-  console.log('message', message)
   return message;
 };
 
--- src/containers/ProjectCard/ProjectCard.js
+++ src/containers/ProjectCard/ProjectCard.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import './ProjectCard.css'
-import Palette from '../Palette/Palette'
+import React from 'react';
+import { connect } from 'react-redux';
+import './ProjectCard.css';
+import Palette from '../Palette/Palette';
 
 
 export const ProjectCard = ({ name, id, palettes }) => {
-  const findMatchingPalettes = palettes.filter(palette => {
-   return palette.project_id === id
-  })
+  const findMatchingPalettes = palettes.filter((palette) => {
+    return palette.project_id === id;
+  });
   const displayPalettes = findMatchingPalettes.map((palette, index) => {
     return (
       <Palette
@@ -15,21 +15,21 @@ export const ProjectCard = ({ name, id, palettes }) => {
         key={index}
         className='palette-card'
        />
-    )
-  })
+    );
+  });
 
   return (
     <section className='project-card'>
       <h4 className='project-name'>{name}</h4>
       {displayPalettes}
     </section>
-  )
-}
+  );
+};
 
 
 export const mapState = (state) => ({
   palettes: state.palettes,
-  projects: state.projects
-})
+  projects: state.projects,
+});
 
-export default connect (mapState)(ProjectCard)
+export default connect(mapState)(ProjectCard);
