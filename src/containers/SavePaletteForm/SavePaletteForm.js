8b010a8b38e667855ed49da7ca7d0fb3c80a2a79
Brady Bridges <bradyjbridges@gmail.com>
2019-12-10 20:30:03 -0700

    Add hover state

--- src/containers/SavePaletteForm/SavePaletteForm.js
+++ src/containers/SavePaletteForm/SavePaletteForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addPalette } from '../../actions/index';
-import './SavePaletteForm.css'
+import './SavePaletteForm.scss'
 
 export class SavePaletteForm extends Component {
   constructor() {
