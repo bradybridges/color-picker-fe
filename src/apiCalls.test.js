762b541ec38bc157e5fe744c441ee69f0c453d94
Sara <saralkarsh@gmail.com>
2019-12-11 13:06:02 -0700

    Add deleteProject tests

--- src/apiCalls.test.js
+++ src/apiCalls.test.js
@@ -337,57 +337,83 @@ describe('postProject', () => {
 
     expect(result).rejects.toEqual(Error('Failed to post'))
   })
-
 })
 
 
-// describe('postPalette', () => {
-//   const mockPalette = {
-//     projectId: 2,
-//     paletteName: 'Great Palette',
-//     color1: '#000000',
-//     color2: '#FFFFFF',
-//     color3: '#1F1F1F',
-//     color4: '#1D1D1D',
-//     color5: '#CCCCCC',
-//   };
+describe('deleteProject', () => {
+  const mockProject = {id: 1, name: 'a project'}
+
+  beforeEach(() => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve('Successfully deleted project')
+      })
+    })
+  })
+
+  it('should be called with the correct URL', () => {
+    const { id } = mockProject
+    const expectedUrl = `https://color-picker-backend.herokuapp.com/api/v1/projects/1`
+
+    const expectedBody = {method: 'DELETE'}
+
+    api.deleteProject(id)
+
+    expect(window.fetch).toHaveBeenCalledWith(expectedUrl, expectedBody)
+  })
+
+  it('should return a success message if the delete succeeds', () => {
+    const result = api.deleteProject(1)
+    expect(result).resolves.toEqual('Successfully deleted project')
+  })
+
+  it('should return an error if the repsonse is not ok', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolves({ ok: false })
+    })
+    const result = api.deleteProject(1)
+    expect(result).rejects.toEqual(Error('Failed to delete project'))
+  })
+
+  it('should return an error if the fetch fails', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject(Error('fetch failed'))
+    })
+    const result = api.deleteProject(1)
+
+    expect(result).rejects.toEqual(Error('fetch failed'))
+  })
+})
+
+// describe('deletePalette', () => {
 //   beforeEach(() => {
 //     window.fetch = jest.fn().mockImplementation(() => {
 //       return Promise.resolve({
 //         ok: true,
-//         json: () => Promise.resolve('Successfully added palette'),
+//         json: () => Promise.resolve('Successfully deleted palette'),
 //       });
 //     });
 //   });
-//   it('should be called with the correct url and body', () => {
-//     const { projectId, paletteName, color1, color2, color3, color4, color5 } = mockPalette;
-//     const expectedUrl = 'https://color-picker-backend.herokuapp.com/api/v1/palettes';
-//     const expectedBody = {
-//       "body": "{\"project_id\":2,\"palette_name\":\"Great Palette\",\"color_1\":\"#000000\",\"color_2\":\"#FFFFFF\",\"color_3\":\"#1F1F1F\",\"color_4\":\"#1D1D1D\",\"color_5\":\"#CCCCCC\"}",
-//       "headers": {
-//         "Content-Type": "application/json",
-//       },
-//       "method": "POST",
-//     };
-//     api.postPalette(projectId, paletteName, color1, color2, color3, color4, color5);
+//   it('should be called with the correct url', () => {
+//     const expectedUrl = 'https://color-picker-backend.herokuapp.com/api/v1/palettes/1';
+//     const expectedBody = { method: 'DELETE' };
+//     api.deletePalette(1);
 //     expect(window.fetch).toHaveBeenCalledWith(expectedUrl, expectedBody);
 //   });
-//   it('should return a successful message if palette posts', () => {
-//     const result = api.postPalette(mockPalette);
-//     expect(result).resolves.toEqual('Successfully added palette');
+//   it('should return a success message if deletion succeeds', () => {
+//     const result = api.deletePalette(1);
+//     expect(result).resolves.toEqual('Successfully deleted palette');
 //   });
-//   it('should return an error if the fetch fails', () => {
-//     window.fetch = jest.fn().mockImplementation(() => {
-//       return Promise.resolve({ ok: false });
-//     });
-//     const result = api.postPalette();
-//     expect(result).rejects.toEqual(Error('Failed to POST palette'));
+//   it('should return an error if response is not okay', () => {
+//     window.fetch = jest.fn().mockImplementation(() => Promise.resolve({ ok: false }));
+//     const result = api.deletePalette(1);
+//     expect(result).rejects.toEqual(Error('Failed to delete palette'));
 //   });
 //   it('should return an error if the promise rejects', () => {
-//     window.fetch = jest.fn().mockImplementation(() => {
-//       return Promise.reject(Error('Failed to post'));
-//     });
-//     const result = api.postPalette();
-//     expect(result).rejects.toEqual(Error('Failed to post'));
+//     window.fetch = jest.fn().mockImplementation(() => Promise.reject(Error('Failed to delete palette')));
+//     const result = api.deletePalette(1);
+//     expect(result).rejects.toEqual(Error('Failed to delete palette'));
 //   });
 // });
+
